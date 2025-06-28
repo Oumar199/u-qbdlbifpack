e86a437e736ae836e8ca34faf2d751e6ad9a635f
Oumar Kane <oumar.kane@univ-thies.sn>
2025-06-28 11:25:30 +0000

    adding avg gradient

--- translate_package/models/gradient_observation.py
+++ translate_package/models/gradient_observation.py
@@ -6,4 +6,4 @@ def get_gradients_mean(model: Module):
         if param.grad is not None:
             ave_grads.append(param.grad.abs().mean().item())
     
-    return sum(ave_grads)/len(ave_grads)
+    return sum(ave_grads)/(len(ave_grads) + 1e-5)
